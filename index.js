3443fa648a5f5dfc04e9bbe33874cd47e0514aaf
Chirag Gupta <guptachirag2494@gmail.com>
2021-06-04 13:21:51 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const fs = require("fs")
+// fs.mkdirSync("chirag");
+// fs.writeFileSync("chirag/bio.txt","hello");
+// fs.writeFileSync("bio.txt","hello chirag");
+// fs.appendFileSync("bio.txt", " Good night");
+//  const name=fs.readFileSync("bio.txt","utf-8");
+//  console.log(name)
+//  fs.renameSync("bio.txt","biodata.txt");
+//  fs.unlinkSync("chirag/bio.txt");
+ fs.rmdirSync("chirag");
+ 
